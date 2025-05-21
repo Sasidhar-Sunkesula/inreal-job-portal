71eda6157a1c97acf14bffe220d26c084e06ca2e
Sasidhar Sunkesula <sasidharsunkesula579@gmail.com>
2025-05-21 17:36:49 +0530

    Fixes jobs routes

--- backend/src/middleware/addUserId.ts
+++ backend/src/middleware/addUserId.ts
@@ -6,6 +6,10 @@ export function addUserId(req: Request, res: Response, next: NextFunction) {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined");
   }
+  if (!token) {
+    next();
+    return;
+  }
   const decoded = jwt.verify(token, process.env.JWT_SECRET) as { id: string };
   req.userId = decoded.id;
   next();
